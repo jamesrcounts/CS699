bd923947e0658d49432298d2ba90b8e3765ef188
Jim Counts <jamesrcounts@outlook.com>
2013-01-26 08:04:11 -0800

    drew clouds

--- sth/game.js
+++ sth/game.js
@@ -1,4 +1,4 @@
-var clear = function (ctx) {    
+var clear = function (ctx, width, height) {
     // draw rectangle same size as canvas
     ctx.beginPath();
     ctx.rect(0, 0, width, height);
@@ -9,15 +9,44 @@ var clear = function (ctx) {
     ctx.fill();
 }
 
+var drawClouds = function (ctx, clouds, count) {
+    for (var i = 0; i < count; i++) {
+        var cloud = clouds[i];
+        // draw a circle
+        ctx.beginPath();
+        ctx.arc(cloud.x, cloud.y, cloud.radius, 0, Math.PI * 2, true);
+        ctx.closePath();
+
+        // fill it with semi-transparent white
+        ctx.fillStyle = 'rgba(255,255,255,' + cloud.opacity + ')';
+        ctx.fill();
+    }
+}
+
+var makeCloud = function (width, height) {
+    return {
+        x: Math.random() * width,
+        y: Math.random() * height,
+        radius: Math.random() * 100,
+        opacity: Math.random() / 2
+    };
+}
+
+var gameProperties = {
+    width: "320",
+    height: "500",
+    maxClouds: "10"
+};
+
 var game = document.getElementById('game');
+game.width = gameProperties.width;
+game.height = gameProperties.height;
 
-var width = 320;
-game.width = width;
-
-var height = 500;
-game.height = height;
+var clouds = [];
+for (var i = 0; i < gameProperties.maxClouds; i++) {
+    clouds.push(makeCloud(gameProperties.width, gameProperties.height));
+}
 
 var gameContext = game.getContext('2d');
-
-clear(gameContext);
-
+clear(gameContext, gameProperties.width, gameProperties.height);
+drawClouds(gameContext, clouds, gameProperties.maxClouds);
