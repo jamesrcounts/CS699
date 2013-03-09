f0acb8f35ed5ed24d8b2b16e49d3243aa281446c
Jim Counts <jamesrcounts@outlook.com>
2013-03-09 07:34:28 -0800

    Moved tangles to files.

---
+++ stairway/src/tangles.js
@@ -0,0 +1,136 @@
+Tangle.classes.CarouselText = {
+    initialize: function (element, options, tangle, variable) {
+        var carousel = this;
+        carousel.values = (options.values !== undefined) ? options.values.split(" ") : ["locked"];
+
+        element.onclick = function () {
+            var value = tangle.getValue(variable);
+            var key = carousel.values.indexOf(value);
+            var next = (key + 1) % carousel.values.length;
+            tangle.setValue(variable, carousel.values[next]);
+        };
+    }
+};
+Tangle.classes.DragText = {
+    cursorDragClass: "TKCursorDragHorizontal",
+    dragClass: "TKAdjustableNumberDown",
+
+    initialize: function (element, options, tangle, variable) {
+        this.element = element;
+        this.values = this.getValues(options.values);
+
+        this.tangle = tangle;
+        this.variable = variable;
+
+        this.initializeHover();
+        this.initializeHelp();
+        this.initializeDrag();
+    },
+
+    getValues: function (values_data) {
+        if (values_data !== undefined) {
+            return values_data.split(" ");
+        }
+
+        return ["locked"];
+    },
+
+    hoverClass: "TKAdjustableNumberHover",
+
+    initializeDrag: function () {
+        this.isDragging = false;
+        new BVTouchable(this.element, this);
+    },
+
+    initializeHelp: function () {
+        this.helpElement = (new Element("div", {
+            "class": "TKAdjustableNumberHelp"
+        })).inject(this.element, "top");
+        this.helpElement.setStyle("display", "none");
+        this.helpElement.set("text", "drag");
+    },
+
+    initializeHover: function () {
+        this.isHovering = false;
+        this.element.addEvent("mouseenter", (function () {
+            this.isHovering = true;
+            this.updateRolloverEffects();
+        }).bind(this));
+        this.element.addEvent("mouseleave", (function () {
+            this.isHovering = false;
+            this.updateRolloverEffects();
+        }).bind(this));
+    },
+
+    isActive: function () {
+        return this.isDragging || this.isHovering;
+    },
+
+    touchDidGoDown: function (touches) {
+        this.valueAtMouseDown = this.tangle.getValue(this.variable);
+        this.isDragging = true;
+        this.updateRolloverEffects();
+        this.updateStyle();
+    },
+
+    touchDidMove: function (touches) {
+        var key = this.values.indexOf(this.valueAtMouseDown) + touches.translation.x / 10;
+        key = (key.round()).limit(0, this.values.length - 1);
+        var value = this.values[key];
+
+        this.tangle.setValue(this.variable, value);
+        this.updateHelp();
+    },
+
+    touchDidGoUp: function (touches) {
+        this.helpElement.setStyle("display", "none");
+        this.isDragging = false;
+        this.updateRolloverEffects();
+        this.updateStyle();
+    },
+
+    updateCursor: function () {
+        var body = document.getElement("body");
+        if (this.isActive()) {
+            body.addClass(this.cursorDragClass);
+        } else {
+            body.removeClass(this.cursorDragClass);
+        }
+    },
+
+    updateHelp: function () {
+        var size = this.element.getSize();
+        var top = -size.y + 7;
+        var left = Math.round(0.5 * (size.x - 20));
+        var display = "none";
+        if (this.isHovering) {
+            display = "block";
+        }
+
+        this.helpElement.setStyles({
+            left: left,
+            top: top,
+            display: display
+        });
+    },
+
+    updateRolloverEffects: function () {
+        this.updateStyle();
+        this.updateCursor();
+        this.updateHelp();
+    },
+    updateStyle: function () {
+        if (this.isDragging) {
+            this.element.addClass(this.dragClass);
+        } else {
+            this.element.removeClass(this.dragClass);
+        }
+
+        if (!this.isDragging && this.isActive()) {
+            this.element.addClass(this.hoverClass);
+        } else {
+            this.element.removeClass(this.hoverClass);
+        }
+    }
+
+};
