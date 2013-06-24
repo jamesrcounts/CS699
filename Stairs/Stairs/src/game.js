9674f58bc5d6842a72982afe200729c3424903eb
Jim Counts <jamesrcounts@outlook.com>
2013-06-23 21:30:33 -0700

    figuring out window

--- Stairs/Stairs/src/game.js
+++ Stairs/Stairs/src/game.js
@@ -1,79 +1,98 @@
 ﻿"use strict";
 
-var game = (function () {
-    var board, player, controls;
-    board = createBoard('game');
-    board.addPiece(createBackground(board));
-    board.addPieces(10, createCloud);
+function _game() {
+    var canvas
+    , stage
+    , world;
 
-    board.addPieces(7, createPlatform, evenVerticalSpacing);
+    canvas = document.getElementById('game');
+    canvas.width = 320;
+    canvas.height = 500;
 
-    player = createPlayer(board);
-    controls = createKeyboardControls();
-    controls.moveLeft = function () {
-        player.moveLeft();
-    };
-    controls.moveRight = function () {
-        player.moveRight();
-    };
-    controls.moveUp = function () {
-        player.jump();
-    };
-    board.addPiece(player);
+    stage = new Stage(canvas);
+    world = new Container();
+    stage.addChild(world);
 
-    var hBoundary = {
-        scale: board.scale,
-        width: board.width(),
-        height: 0.5,
-        x: board.width() / 2,
-        y: board.height() - 1,
-        id: "ground"
-    };
-    var ground = createBoundary(hBoundary);
-    board.addPiece(ground);
-    hBoundary.y = 1;
-    hBoundary.id = "roof";
-    board.addPiece(createBoundary(hBoundary));
 
-    var vBoundary = {
-        scale: 30,
-        width: 0.5,
-        height: board.height(),
-        x: 1,
-        y: board.height() / 2
-    };
-    board.addPiece(createBoundary(vBoundary));
-    vBoundary.x = board.width() - 1;
-    board.addPiece(createBoundary(vBoundary));
 
-    board.addContactListener(createContactListener());
+}
 
-    //board.debug();
+new _game();
 
-    createjs.Ticker.setFPS(60);
-    createjs.Ticker.useRAF = true;
-    var update = function () {
-        board.update();
-    };
+//var game = (function () {
+//    var board, player, controls;
+//    board = createBoard('game');
+//    board.addPiece(createBackground(board));
+//    board.addPieces(10, createCloud);
 
-    createjs.Ticker.addEventListener("tick", update);
-    ground.onContact = function () {
-        createjs.Ticker.removeEventListener("tick", update);
-    };
+//    board.addPieces(7, createPlatform, evenVerticalSpacing);
 
-    return {
-        boardSize: function (size) {
-            if (board) {
-                board.size(size || "small");
-            }
+//    player = createPlayer(board);
+//    controls = createKeyboardControls();
+//    controls.moveLeft = function () {
+//        player.moveLeft();
+//    };
+//    controls.moveRight = function () {
+//        player.moveRight();
+//    };
+//    controls.moveUp = function () {
+//        player.jump();
+//    };
+//    board.addPiece(player);
 
-            return this;
-        }
-        , playerAgility: function (agility) {
-            if (player) {
-                player.flies(agility || "normally");
-            }
-            return this;
-        }
-    };
-})();
+//    var hBoundary = {
+//        scale: board.scale,
+//        width: board.width(),
+//        height: 0.5,
+//        x: board.width() / 2,
+//        y: board.height() - 1,
+//        id: "ground"
+//    };
+//    var ground = createBoundary(hBoundary);
+//    board.addPiece(ground);
+//    hBoundary.y = 1;
+//    hBoundary.id = "roof";
+//    board.addPiece(createBoundary(hBoundary));
+
+//    var vBoundary = {
+//        scale: 30,
+//        width: 0.5,
+//        height: board.height(),
+//        x: 1,
+//        y: board.height() / 2
+//    };
+//    board.addPiece(createBoundary(vBoundary));
+//    vBoundary.x = board.width() - 1;
+//    board.addPiece(createBoundary(vBoundary));
+
+//    board.addContactListener(createContactListener());
+
+//    //board.debug();
+
+//    createjs.Ticker.setFPS(60);
+//    createjs.Ticker.useRAF = true;
+//    var update = function () {
+//        board.update();
+//    };
+
+//    createjs.Ticker.addEventListener("tick", update);
+//    ground.onContact = function () {
+//        createjs.Ticker.removeEventListener("tick", update);
+//    };
+
+//    return {
+//        boardSize: function (size) {
+//            if (board) {
+//                board.size(size || "small");
+//            }
+
+//            return this;
+//        }
+//        , playerAgility: function (agility) {
+//            if (player) {
+//                player.flies(agility || "normally");
+//            }
+//            return this;
+//        }
+//    };
+//})();
